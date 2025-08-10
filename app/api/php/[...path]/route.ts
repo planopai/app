80065160bf2f0b711bd0732dbf0b1c8e83975ae6
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-10 14:44:59 -0300

    Primeiro

--- app/api/php/[...path]/route.ts
+++ app/api/php/[...path]/route.ts
@@ -27,7 +27,7 @@ function pickForwardHeaders(req: NextRequest) {
     out.set("accept-encoding", "identity");
 
     // Identificação opcional
-    out.set("x-proxy-from", "pai.app");
+    out.set("x-proxy-from", "pai");
     return out;
 }
 
