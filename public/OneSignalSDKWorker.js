eb04d3677eb858a21273c2722b9aa6a3011c5bce
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-11 12:08:32 -0300

    Primeiro commit

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,4 +1,2 @@
-// mantém seu SW do PWA ativo junto com o OneSignal
 try { importScripts('/sw.js'); } catch (_) { }
-// OneSignal v16 worker
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
