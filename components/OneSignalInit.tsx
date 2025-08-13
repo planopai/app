8e845f3c8c82ee4751a152f35a744459998fe14e
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-13 11:51:34 -0300

    Primeiro commit

---
+++ components/OneSignalInit.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
+
+export default function OneSignalInit() {
+    useEffect(() => {
+        (async () => {
+            await OneSignal.init({
+                appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!,
+                notifyButton: {
+                    enable: true,
+                    prenotify: true,
+                    showCredit: false,
+                    position: "bottom-right",
+                    text: {
+                        "tip.state.unsubscribed": "Ativar notificações",
+                        "tip.state.subscribed": "Notificações ativadas",
+                        "tip.state.blocked": "Notificações bloqueadas",
+                        "message.prenotify": "Clique para ativar as notificações",
+                        "message.action.subscribing": "Inscrevendo...",
+                        "message.action.subscribed": "Inscrição concluída!",
+                        "message.action.resubscribed": "Inscrito novamente",
+                        "message.action.unsubscribed": "Você não receberá notificações",
+                        "dialog.main.title": "Notificações do site",
+                        "dialog.main.button.subscribe": "ATIVAR",
+                        "dialog.main.button.unsubscribe": "DESATIVAR",
+                        "dialog.blocked.title": "Notificações bloqueadas",
+                        "dialog.blocked.message": "Permita as notificações nas configurações do navegador"
+                    }
+                },
+                serviceWorkerParam: { scope: "/onesignal/" },
+                serviceWorkerPath: "onesignal/OneSignalSDKWorker.js",
+            });
+        })();
+    }, []);
+
+    return null;
+}
