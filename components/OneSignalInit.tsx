58d1f5bd893b41ac53e60e00ca4bb3db61e8664f
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-13 16:15:21 -0300

    Primeiro commit

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -31,8 +31,8 @@ export default function OneSignalInit() {
                             "dialog.blocked.message": "Permita as notificações nas configurações do navegador"
                         }
                     },
-                    serviceWorkerPath: "/OneSignalSDKWorker.js",
-                    serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
+                    serviceWorkerPath: "OneSignalSDKWorker.js",
+                    serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
                     serviceWorkerParam: { scope: "/" }, // ✅ escopo raiz
                     allowLocalhostAsSecureOrigin: true, // útil para testes locais
                 });
