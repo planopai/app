742911744cae56dab021931ab874c7309ac4d849
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-13 16:28:08 -0300

    Primeiro commit

--- components/OneSignalInit.tsx
+++ components/OneSignalInit.tsx
@@ -7,6 +7,8 @@ export default function OneSignalInit() {
     useEffect(() => {
         if (typeof window !== "undefined") {
             (async () => {
+                const noCacheSuffix = `?v=${Date.now()}`; // força sempre baixar novo
+
                 await OneSignal.init({
                     appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID!,
                     safari_web_id: process.env.NEXT_PUBLIC_ONESIGNAL_SAFARI_ID!,
@@ -31,10 +33,10 @@ export default function OneSignalInit() {
                             "dialog.blocked.message": "Permita as notificações nas configurações do navegador"
                         }
                     },
-                    serviceWorkerPath: "OneSignalSDKWorker.js",
-                    serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
-                    serviceWorkerParam: { scope: "/" }, // ✅ escopo raiz
-                    allowLocalhostAsSecureOrigin: true, // útil para testes locais
+                    serviceWorkerPath: `/OneSignalSDKWorker.js${noCacheSuffix}`,
+                    serviceWorkerUpdaterPath: `/OneSignalSDKUpdaterWorker.js${noCacheSuffix}`,
+                    serviceWorkerParam: { scope: "/" },
+                    allowLocalhostAsSecureOrigin: true
                 });
             })();
         }
