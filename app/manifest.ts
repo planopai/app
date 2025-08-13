eb02a785a0d6b6ff5be8900b2b616237a72231c1
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-13 18:57:42 -0300

    Primeiro commit

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,13 +9,32 @@ export default function manifest(): MetadataRoute.Manifest {
         start_url: "/",
         scope: "/",
         display: "standalone",
-        display_override: ["standalone"], // força standalone quando possível
+        display_override: ["standalone"],
         background_color: "#ffffff",
         theme_color: "#059de0",
+        orientation: "portrait",
         icons: [
-            { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-            { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
-            { src: "/icon-512-maskable.png", sizes: "512x512", type: "image/png", purpose: "maskable" }, // opcional, recomendado
-        ],
+            {
+                src: "/icon-192.png",
+                sizes: "192x192",
+                type: "image/png"
+            },
+            {
+                src: "/icon-512.png",
+                sizes: "512x512",
+                type: "image/png"
+            },
+            {
+                src: "/icon-512-maskable.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable"
+            },
+            {
+                src: "/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png"
+            }
+        ]
     };
 }
