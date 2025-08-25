2c265f1ab66f34b70f704f09b0163eb9451d78bd
Plano PAI <planoassistencialintegrado@gmail.com>
2025-08-25 14:36:26 -0300

    Atualização

--- app/acompanhamento/components/FieldLabel.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import React from "react";
-
-export default function FieldLabel({ children }: { children: React.ReactNode }) {
-    return <label className="mb-1 block text-sm font-medium">{children}</label>;
-}
