95dd0d91b5cce30fde436e90ce1cf77b352a41fd
planopai <planoassistencialintegrado@gmail.com>
2026-02-25 12:59:59 -0300

    Sorteios

--- app/sorteios/page.tsx
+++ app/sorteios/page.tsx
@@ -38,7 +38,7 @@ type DashboardResp = {
 
 type ApiResp<T = any> = { ok: boolean } & T;
 
-const API_URL = process.env.NEXT_PUBLIC_SORTEIOS_API_URL || "/sorteios.php";
+const API_URL = process.env.NEXT_PUBLIC_SORTEIOS_API_URL || "https://api.planoassistencialintegrado.com.br/sorteios.php";
 
 function toLocalInputValue(mysqlDatetime: string) {
     if (!mysqlDatetime) return "";
